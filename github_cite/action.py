16ed25931a35da8148d7d90d11aed36de3663c8f
Nicolai Huening <nicolai-huening@gmx.de>
2025-02-19 17:33:22 +0100

    Update output of xml file

--- github_cite/action.py
+++ github_cite/action.py
@@ -1,3 +1,4 @@
+# Python script executed using GitHub actions.
 from citation_translator import GithubRepoDataCite
 from exceptions import GithubException
 import sys
@@ -5,8 +6,8 @@ import os
 
 def set_github_action_output(output_name, output_value):
     f = open(os.path.abspath(os.environ["GITHUB_OUTPUT"]), "a")
-    f.write(f'{output_name}={output_value}')
-    f.close()    
+    f.write(f'{output_name}<<EOF\n{output_value}\nEOF\n')
+    f.close()
 
 if __name__ == "__main__":
     repo_owner = os.environ["INPUT_REPOOWNER"]
@@ -17,7 +18,7 @@ if __name__ == "__main__":
     try:
         data = GithubRepoDataCite(repo_owner, repo_name, githubApiUrl=github_api_url, githubUrl=github_url, barerToken=api_token)
         sys.stdout.write(data.pretty_xml())
-        set_github_action_output('dataCiteXML', data.pretty_xml())
+        set_github_action_output('datacitexml', data.pretty_xml())
         
     except GithubException as e:
         sys.stderr.write(f"An exception occurred:\n{e.message}")
